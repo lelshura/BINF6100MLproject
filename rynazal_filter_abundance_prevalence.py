1d7fc9acbeb80c3f5f2e265cc689a56767b5179b
Neha <148917960+nnehapatel@users.noreply.github.com>
2024-04-12 15:38:17 -0400

    Update rynazal_filter_abundance_prevalence.py
    Fix commenting and organization + use a new set of thresholds to generate a dataset with alternate filtering.

--- rynazal_filter_abundance_prevalence.py
+++ rynazal_filter_abundance_prevalence.py
@@ -1,15 +1,15 @@
-# code adapted from: https://github.com/ryzary/shapmat_paper/blob/v1.0.0/shapmat/abundance_filter.py
+# Code adapted from: https://github.com/ryzary/shapmat_paper/blob/v1.0.0/shapmat/abundance_filter.py
 import pandas as pd
 
 def set_abundance_to_zero(data, abundance_threshold=1e-15):
     """
-    Set abundance that are < threshold to 0
+    Set abundances that are < threshold to 0
     skipping the sample ID, study_name, and CRC columns
     """
-    # identify columns containing relative abundance values
+    # Identify columns containing relative abundance values
     cols_to_convert = data.columns[1:-1]
 
-    # convert relative abundance values to numeric values
+    # Convert relative abundance values to numeric values
     data[cols_to_convert] = data[cols_to_convert].apply(pd.to_numeric)
 
     ab_filtered_1 = data.copy()
@@ -20,14 +20,12 @@ def set_abundance_to_zero(data, abundance_threshold=1e-15):
 
 def ab_filter(data, abundance_threshold=1e-15, prevalence_threshold=0.9):
     """
-    1. Set abundance that are lower than abundance_threshold to 0
-    2. Remove low frequency features. Features with number of zeros > percent_threshold are removed
-    Returns:
-        DataFrame
+    1. Set abundances that are lower than abundance_threshold to 0
+    2. Remove low frequency features. Features with number of zeros > pervalence_threshold are removed
     """
     ab_filtered_1 = set_abundance_to_zero(data, abundance_threshold)
 
-    # identify columns containing relative abundance values
+    # Identify columns containing relative abundance values
     cols_to_filter = data.columns[1:-1]
 
     selected_species = []
@@ -45,25 +43,40 @@ def ab_filter(data, abundance_threshold=1e-15, prevalence_threshold=0.9):
 
     return filtered_data
 
-# read in file containing abundance values
+# Read in file containing abundance values
 df = pd.read_csv("rynazal_data.csv", header=0)
 
-# remove study name column
+# Remove study name column
 df.drop(df.columns[-2], axis=1, inplace=True)
 
-# rename first column to "Sample ID"
+# Rename first column to "Sample ID"
 df.rename(columns={df.columns[0]: "Sample ID"}, inplace=True)
 
-# apply filter functions to the dataframe
+# Check number of control and CRC patients
+num_zeros = (df['CRC'] == 0).sum()
+
+print("Number of CRC samples:",num_zeros)
+print("Number of control samples:",len(df) - num_zeros)
+
+'''
+Use default values of function to filter dataset
+'''
+# Apply filter functions to the dataframe
 filtered_df = ab_filter(df)
 
-# write dataframe to a csv
+# Write dataframe to a csv file
 filtered_df.to_csv("rynazal_filtered_abundance.csv", index = False)
 
 print("Original dataframe size:", df.shape[0], "samples,", df.shape[1], "taxa.")
 print("Filtered dataframe size:", filtered_df.shape[0],"samples,", filtered_df.shape[1], "taxa.")
 
-num_zeros = (filtered_df['CRC'] == 0).sum()
+'''
+Use values from rynazal tutorial to filter dataset
+'''
 
-print("Number of CRC samples:",num_zeros)
-print("Number of control samples:",802 - num_zeros)
+# Apply filter function with adjusted thresholds to dataframe
+filtered_df_new = ab_filter(df, abundance_threshold=1e-7, prevalence_threshold=0.95)
+
+# Write dataframe to a csv file
+filtered_df_new.to_csv("rynazal_filtered_abundance_NEW.csv", index = False)
+print("New filtered dataframe size:", filtered_df_new.shape[0],"samples,", filtered_df_new.shape[1], "taxa.")
